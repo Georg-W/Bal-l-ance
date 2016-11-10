7c15b3b427eac065f70f3692618a2b4d3f1d3321
ruby -v <georg.westner@googlemail.com>
2016-11-10 17:26:09 +0100

    screens added

--- app/src/main/java/com/example/ersgutercomputer/ballance/Level_2.java
+++ app/src/main/java/com/example/ersgutercomputer/ballance/Level_2.java
@@ -6,25 +6,29 @@ import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
+import static android.graphics.Color.BLUE;
 import static android.graphics.Color.RED;
 
 /**
  * Created by ersgutercomputer on 10.11.2016.
  */
 
-public class Level_1 extends View {
+public class Level_2 extends View{
 
-    public Level_1(Context context) {
+
+    public Level_2(Context context) {
         super(context);
     }
 
-    public Level_1(Context context, AttributeSet attrs) {
+    public Level_2(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public Level_1(Context context, AttributeSet attrs, int defStyleAttr) {
+    public Level_2(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
+
+
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
@@ -37,7 +41,7 @@ public class Level_1 extends View {
 
         Paint p = new Paint();
         p.setStyle(Paint.Style.FILL);
-        p.setColor(RED);
+        p.setColor(BLUE);
 
         canvas.drawCircle(50,50,30,p);
 
--- app/src/main/java/com/example/ersgutercomputer/ballance/Start.java
+++ app/src/main/java/com/example/ersgutercomputer/ballance/Start.java
@@ -1,7 +1,10 @@
 package com.example.ersgutercomputer.ballance;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class Start extends AppCompatActivity {
 
@@ -10,4 +13,13 @@ public class Start extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
     }
+
+    void start(View v){
+        Log.d("asd", "test");
+        Intent i = new Intent(this, GameView.class);
+        startActivity(i);
+    }
+
+
+
 }
