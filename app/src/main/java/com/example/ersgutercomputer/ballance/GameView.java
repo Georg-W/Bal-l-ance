06050ef427c88af43a83f791308dcfd84fda2955
Georg-W <georg.westner@googlemail.com>
2016-11-12 00:21:01 +0100

    first functioning Sensormovement

--- app/src/main/java/com/example/ersgutercomputer/ballance/GameView.java
+++ app/src/main/java/com/example/ersgutercomputer/ballance/GameView.java
@@ -1,13 +1,48 @@
 package com.example.ersgutercomputer.ballance;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class GameView extends AppCompatActivity {
+public class GameView extends AppCompatActivity implements SensorEventListener{
+
+    private Sensor mSensor;
+    private SensorManager mSensorManager;
+    Level_1 level1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game_view);
+        level1 = new Level_1(this);
+        setContentView(level1);
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, mSensor,
+                SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        level1.changePosition(event.values[0], event.values[1]);
+        level1.invalidate();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mSensor,
+                SensorManager.SENSOR_DELAY_GAME);
+    }
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
     }
 }
--- app/src/main/java/com/example/ersgutercomputer/ballance/Level_1.java
+++ app/src/main/java/com/example/ersgutercomputer/ballance/Level_1.java
@@ -3,8 +3,14 @@ package com.example.ersgutercomputer.ballance;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 
 import static android.graphics.Color.RED;
 
@@ -14,8 +20,15 @@ import static android.graphics.Color.RED;
 
 public class Level_1 extends View {
 
+    private int x;
+    private int y;
+    private ShapeDrawable bubble;
+    private int diameter;
+
+
     public Level_1(Context context) {
         super(context);
+        createBubble();
     }
 
     public Level_1(Context context, AttributeSet attrs) {
@@ -31,17 +44,29 @@ public class Level_1 extends View {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 
+
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        Paint p = new Paint();
-        p.setStyle(Paint.Style.FILL);
-        p.setColor(RED);
+        bubble.draw(canvas);
+    }
 
-        canvas.drawCircle(50,50,30,p);
+    void changePosition(float a, float b){
+        x = (int) (x + a);
+        y = (int) (y + b);
+        Log.d("test", ""+x+" "+y);
+        bubble.setBounds(x, y, x + diameter, y + diameter);
+    }
 
+    private void createBubble() {
 
-
+        x = 200;
+        y = 300;
+        diameter = 100;
+        bubble = new ShapeDrawable(new OvalShape());
+        bubble.setBounds(x, y, x + diameter, y + diameter);
+        bubble.getPaint().setColor(0xff74AC23);
     }
 }
